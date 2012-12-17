108e308636d772dbb00f49719ca8349802ff4d7e
hanson <hangzhong.yu@gmail.com>
2012-12-17 17:02:56 +0800

    156

--- 156/3.cpp
+++ 156/3.cpp
@@ -1,51 +1,38 @@
 #include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <map>
 using namespace std;
 
-const long maxn = 4005;
-long b[maxn];
-long n;
-long dp[maxn][2] = {0};//dp[][0] is len, dp[][1] is q
+int hashMap[4003];
+map<int,int> hMap;
+int hashCnt;
 
-int main()
-{
-    cin>>n;
-    cin>>b[1];
-    dp[1][0] = 1;
+int mymap[4003][4003];
+int array[4003];
 
-    for (long i = 2; i <= n; ++i)
-    {
-        cin>>b[i];
-        dp[i][0] = 1;
-
-        long temp = dp[1][0];
-        dp[i][1] = b[1] - b[i];
-
-        for (long k = 2; k < i; ++k)
-        {   
-            long tdp = (b[i]-b[k]);
-            if (dp[k][0] % 2 != 0)
-            {
-                tdp = -tdp;
-            }
-
-            if (dp[k][1] == tdp && dp[k][0]>temp)
-            {
-                temp = dp[k][0];
-                dp[i][1] = tdp;
-            }
-        }
-
-        dp[i][0] += temp;
-    }
-
-    long ans = 0;
-    for (long i = 1; i <= n; ++i)
-    {
-        if (dp[i][0] > ans)
-        {
-            ans = dp[i][0];
-        }
-    }
-    cout<<ans<<endl;
+int hash(int n){
+    for(map<int,int>::const_iterator iter=hMap.begin();iter!=hMap.end();++iter)
+        if(iter->first==n)
+            return iter->second;
+    hMap[n]=hashCnt;
+    return hashCnt++;
 }
-
+int main(){
+    hashCnt=0;
+    int mx=0;
+    int n;
+    cin>>n;
+    for(int i=0;i<n;++i){
+        cin>>array[i];
+        array[i]=hash(array[i]);
+    }
+    memset(mymap,0,sizeof(mymap));
+    for(int i=0;i<n;++i){
+        for(int j=0;j<hashCnt;++j){
+            mx=max(mx,mymap[j][array[i]]=mymap[array[i]][j]+1);
+        }
+    }
+    cout<<mx<<endl;
+    return 0;
+}
